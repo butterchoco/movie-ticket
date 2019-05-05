7c72fb8512a07ebb652ede35241b7772d994c2e4
M. Fairuzi Teguh <mfairuzit@gmail.com>
2019-05-05 15:39:36 +0900

    Split movie controller into two controllers

--- movie/src/main/java/com/adpro/movie/MovieController.java
+++ movie/src/main/java/com/adpro/movie/MovieController.java
@@ -7,9 +7,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
@@ -33,14 +34,14 @@ public class MovieController {
         return new RedirectView("/movies");
     }
 
-    @RequestMapping("/movies")
+    @GetMapping("/movies")
     public String movies(Model model) {
         List<Movie> movies = movieListProxy.findMoviesByReleaseDateAfter(LocalDate.now().minusDays(7));
         model.addAttribute("movies", movies);
         return "movies";
     }
 
-    @RequestMapping("/movie/{movieId}")
+    @GetMapping("/movie/{movieId}")
     public String movieSessions(@PathVariable Long movieId, Model model) {
         LocalDateTime midnight = LocalDateTime.of(LocalDate.now(), LocalTime.MIDNIGHT);
         List<MovieSession> movieSessions = movieSessionRepository.findMovieSessionsByMovieIdAndStartTimeAfter(movieId, midnight);
