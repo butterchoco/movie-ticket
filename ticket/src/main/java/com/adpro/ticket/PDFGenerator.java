b206ddbcb8594cc886ff72cf91501f78c1f3f606
josedevian <jdevian98@gmail.com>
2019-05-08 16:43:48 +0700

    test masih kosong tapi

--- ticket/src/main/java/com/adpro/ticket/PDFGenerator.java
+++ ticket/src/main/java/com/adpro/ticket/PDFGenerator.java
@@ -6,12 +6,16 @@ import java.io.FileOutputStream;
 
 import java.io.IOException;
 
+import com.itextpdf.text.Element;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.Rectangle;
 import com.itextpdf.text.pdf.PdfWriter;
 import com.itextpdf.text.pdf.PdfPTable;
+
+import model.Booking;
+import model.Ticket;
 
 public class PDFGenerator {
     public static void main(String[] args) throws DocumentException, IOException {
@@ -32,54 +36,13 @@ public class PDFGenerator {
 
             Font fontHead = new Font(Font.HELVETICA, 30, Font.BOLDITALIC, Color.RED);
             Font fontSubHead = new Font(FONT.HELVETICA, 20, Font.BOLD, Color.BLACK);
+            Font fontAudi = new Font(FONT.HELVETICA, 60, Font.BOLD, Color.BLACK);
 
-            PdfPCell cellMovie = new PdfPCell(new Paragraph("Movie:", fontSubHead));
-            cellMovie.setBorderColor(BaseColor.WHITE);
-            cellMovie.setPaddingRight(10);
-            cellMovie.setHorizontalAlignment(Element.ALIGN_RIGHT);
-            cellMovie.setVerticalAlignment(Element.ALIGN_MIDDLE);
-
-            PdfPCell cellMovieTitle = new PdfPCell(new Paragraph(ticket.getMovie(), fontSubHead));
-            cellMovieTitle.setBorderColor(BaseColor.WHITE);
-            cellMovieTitle.setPaddingRight(10);
-            cellMovieTitle.setHorizontalAlignment(Element.ALIGN_CENTER);
-            cellMovieTitle.setVerticalAlignment(Element.ALIGN_MIDDLE);
-
-            PdfPCell cellShowtime = new PdfPCell(new Paragraph("Showtime:", fontSubHead));
-            cellShowtime.setBorderColor(BaseColor.WHITE);
-            cellShowtime.setPaddingRight(10);
-            cellShowtime.setHorizontalAlignment(Element.ALIGN_RIGHT);
-            cellShowtime.setVerticalAlignment(Element.ALIGN_MIDDLE);
-
-            PdfPCell cellDate = new PdfPCell(new Paragraph(ticket.getShowtime(), fontSubHead));
-            cellDate.setBorderColor(BaseColor.WHITE);
-            cellDate.setPaddingRight(10);
-            cellDate.setHorizontalAlignment(Element.ALIGN_CENTER);
-            cellDate.setVerticalAlignment(Element.ALIGN_MIDDLE);
-
-            PdfPCell cellSeat = new PdfPCell(new Paragraph("Seat/Row:", fontSubHead));
-            cellSeat.setBorderColor(BaseColor.WHITE);
-            cellSeat.setPaddingRight(10);
-            cellSeat.setHorizontalAlignment(Element.ALIGN_RIGHT);
-            cellSeat.setVerticalAlignment(Element.ALIGN_MIDDLE);
-
-            PdfPCell cellSeatInfo = new PdfPCell(new Paragraph(ticket.getSeat(), fontSubHead));
-            cellSeatInfo.setBorderColor(BaseColor.WHITE);
-            cellSeatInfo.setPaddingRight(10);
-            cellSeatInfo.setHorizontalAlignment(Element.ALIGN_CENTER);
-            cellSeatInfo.setVerticalAlignment(Element.ALIGN_MIDDLE);
-
-            PdfPCell cellAuditorium = new PdfPCell(new Paragraph("Auditorium:", fontSubHead));
-            cellSeat.setBorderColor(BaseColor.WHITE);
-            cellSeat.setPaddingRight(10);
-            cellSeat.setHorizontalAlignment(Element.ALIGN_RIGHT);
-            cellSeat.setVerticalAlignment(Element.ALIGN_MIDDLE);
-
-            PdfPCell cellAudiInfo = new PdfPCell(new Paragraph(ticket.getAudi(), fontSubHead));
-            cellSeatInfo.setBorderColor(BaseColor.WHITE);
-            cellSeatInfo.setPaddingRight(10);
-            cellSeatInfo.setHorizontalAlignment(Element.ALIGN_CENTER);
-            cellSeatInfo.setVerticalAlignment(Element.ALIGN_MIDDLE);
+            generateMovieCell();
+            generateMovieDateCell();
+            generateShowtimeCell();
+            generateSeatInfoCell();
+            generateAudiInfoCell();
 
             document.addAuthor("C-8 Advance Programming");
             document.addCreationDate();
@@ -91,5 +54,75 @@ public class PDFGenerator {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
+    }
+
+    public void generateMovieCell(){
+        PdfPCell cellMovie = new PdfPCell(new Paragraph("Movie:", fontSubHead));
+        cellMovie.setBorderColor(BaseColor.WHITE);
+        cellMovie.setPaddingRight(10);
+        cellMovie.setHorizontalAlignment(Element.ALIGN_RIGHT);
+        cellMovie.setVerticalAlignment(Element.ALIGN_MIDDLE);
+
+        PdfPCell cellMovieTitle = new PdfPCell(new Paragraph(ticket.getMovie(), fontSubHead));
+        cellMovieTitle.setBorderColor(BaseColor.WHITE);
+        cellMovieTitle.setPaddingRight(10);
+        cellMovieTitle.setHorizontalAlignment(Element.ALIGN_CENTER);
+        cellMovieTitle.setVerticalAlignment(Element.ALIGN_MIDDLE);
+    }
+
+    public void generateShowtimeCell() {
+        PdfPCell cellShowtime = new PdfPCell(new Paragraph("Showtime:", fontSubHead));
+        cellShowtime.setBorderColor(BaseColor.WHITE);
+        cellShowtime.setPaddingRight(10);
+        cellShowtime.setHorizontalAlignment(Element.ALIGN_RIGHT);
+        cellShowtime.setVerticalAlignment(Element.ALIGN_MIDDLE);
+
+        PdfPCell cellTime = new PdfPCell(new Paragraph(ticket.getShowtime(), fontSubHead));
+        cellDate.setBorderColor(BaseColor.WHITE);
+        cellDate.setPaddingRight(10);
+        cellDate.setHorizontalAlignment(Element.ALIGN_CENTER);
+        cellDate.setVerticalAlignment(Element.ALIGN_MIDDLE);
+    }
+
+    public void generateMovieDateCell() {
+        PdfPCell cellShowtime = new PdfPCell(new Paragraph("Date:", fontSubHead));
+        cellShowtime.setBorderColor(BaseColor.WHITE);
+        cellShowtime.setPaddingRight(10);
+        cellShowtime.setHorizontalAlignment(Element.ALIGN_RIGHT);
+        cellShowtime.setVerticalAlignment(Element.ALIGN_MIDDLE);
+
+        PdfPCell cellTime = new PdfPCell(new Paragraph(ticket.getDate(), fontSubHead));
+        cellDate.setBorderColor(BaseColor.WHITE);
+        cellDate.setPaddingRight(10);
+        cellDate.setHorizontalAlignment(Element.ALIGN_CENTER);
+        cellDate.setVerticalAlignment(Element.ALIGN_MIDDLE);
+    }
+
+    public void generateSeatInfoCell() {
+        PdfPCell cellSeat = new PdfPCell(new Paragraph("Seat/Row:", fontSubHead));
+        cellSeat.setBorderColor(BaseColor.WHITE);
+        cellSeat.setPaddingRight(10);
+        cellSeat.setHorizontalAlignment(Element.ALIGN_RIGHT);
+        cellSeat.setVerticalAlignment(Element.ALIGN_MIDDLE);
+
+        PdfPCell cellSeatInfo = new PdfPCell(new Paragraph(ticket.getSeat(), fontSubHead));
+        cellSeatInfo.setBorderColor(BaseColor.WHITE);
+        cellSeatInfo.setPaddingRight(10);
+        cellSeatInfo.setHorizontalAlignment(Element.ALIGN_CENTER);
+        cellSeatInfo.setVerticalAlignment(Element.ALIGN_MIDDLE);
+    }
+
+    public void generateAudiInfoCell() {
+        PdfPCell cellAuditorium = new PdfPCell(new Paragraph("Auditorium:", fontSubHead));
+        cellSeat.setBorderColor(BaseColor.WHITE);
+        cellSeat.setPaddingRight(10);
+        cellSeat.setHorizontalAlignment(Element.ALIGN_RIGHT);
+        cellSeat.setVerticalAlignment(Element.ALIGN_MIDDLE);
+
+        PdfPCell cellAudiInfo = new PdfPCell(new Paragraph(ticket.getAudi(), fontAudi));
+        cellSeatInfo.setBorderColor(BaseColor.WHITE);
+        cellSeatInfo.setPaddingRight(10);
+        cellSeatInfo.setHorizontalAlignment(Element.ALIGN_CENTER);
+        cellSeatInfo.setVerticalAlignment(Element.ALIGN_MIDDLE);
     }
 }
---
+++ ticket/src/test/java/com/adpro/ticket/PDFGeneratorTest.java
@@ -0,0 +1,33 @@
+package com.adpro.ticket;
+
+package com.adpro.purchase;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import com.adpro.ticket.model.Booking;
+import com.adpro.ticket.model.Ticket;
+import com.adpro.ticket.web.PDFGenerator;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import org.junit.Test;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.parser.PdfReaderContentParser;
+import com.itextpdf.text.pdf.parser.SimpleTextExtractionStrategy;
+import com.itextpdf.text.pdf.parser.TextExtractionStrategy;
+
+public class PDFGeneratorTest {
+
+}
