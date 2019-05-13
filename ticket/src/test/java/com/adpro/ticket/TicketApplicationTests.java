e96d069ea4dea103c89ed0faf78a2392889606e1
Raihan Ramadistra <ramadistra@gmail.com>
2019-05-13 19:55:02 +0700

    Fix tests

--- ticket/src/test/java/com/adpro/ticket/TicketApplicationTests.java
+++ ticket/src/test/java/com/adpro/ticket/TicketApplicationTests.java
@@ -1,14 +1,18 @@
 package com.adpro.ticket;
 
+import com.adpro.ticket.api.EmailClient;
+import com.adpro.ticket.api.MessageResponse;
+import com.adpro.ticket.api.Movie;
 import com.adpro.ticket.api.MovieService;
+import com.adpro.ticket.api.MovieSession;
 import com.adpro.ticket.api.TicketRequestModel;
 import com.adpro.ticket.model.Booking;
 import com.adpro.ticket.model.Ticket;
 import com.adpro.ticket.repository.BookingRepository;
-import com.adpro.ticket.repository.TicketRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,7 +21,12 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.awt.print.Book;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Set;
+import java.util.concurrent.CompletableFuture;
 
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -32,9 +41,28 @@ public class TicketApplicationTests {
     @Autowired
     private MockMvc mvc;
     @Autowired
-    private TicketRepository ticketRepository;
-    @Autowired
     private BookingRepository bookingRepository;
+    @MockBean
+    private EmailClient mockEmailClient;
+    @MockBean
+    private MovieService mockMovieService;
+
+    private MovieSession createMovieSession() {
+        Movie movie = Movie.builder()
+                .name("Fairuzi Adventures")
+                .description("Petualangan seorang Fairuzi")
+                .duration(Duration.ofMinutes(111))
+                .posterUrl("sdada")
+                .releaseDate(LocalDate.now())
+                .id(1L)
+                .build();
+        return new MovieSession(movie, LocalDateTime.now());
+    }
+
+    private Booking createBooking(long id, Booking.Status status) {
+        return bookingRepository.save(new Booking(id, status, Set.of(new Ticket("1A")), "ramadistra@gmail.com", 12222));
+    }
+
 
     @Test
     public void testCanOrderSeat() throws Exception {
@@ -46,15 +74,20 @@ public class TicketApplicationTests {
 
     @Test
     public void testCannotOrderBookedSeat() throws Exception {
-        Booking booking = bookingRepository.save(new Booking(1L, Booking.Status.VERIFIED, Set.of(new Ticket("1A")), "ramadistra@gmail.com", 12222));
+        var booking = createBooking(1L, Booking.Status.VERIFIED);
         String json = new ObjectMapper().writeValueAsString(new TicketRequestModel(1L, "1A", "ramadistra@gmail.com", 12222));
         this.mvc.perform(post("/tickets").contentType(MediaType.APPLICATION_JSON).content(json))
                 .andExpect(status().is4xxClientError());
     }
 
+
+
     @Test
     public void testCanVerifyTicket() throws Exception {
-        Booking booking = bookingRepository.save(new Booking(2L, Booking.Status.PENDING, Set.of(new Ticket("1A")), "ramadistra@gmail.com", 12222));
+        Booking booking = createBooking(2L, Booking.Status.PENDING);
+        Mockito.when(mockMovieService.getMovieSessionById(Mockito.any())).thenReturn(CompletableFuture.completedFuture(createMovieSession()));
+        Mockito.when(mockEmailClient.sendEmail(Mockito.any()))
+                .thenReturn(CompletableFuture.completedFuture(new MessageResponse("1", "Success!")));
         this.mvc.perform(post("/tickets/" + booking.getId() + "/verify"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.status", is("VERIFIED")));
@@ -62,7 +95,7 @@ public class TicketApplicationTests {
 
     @Test
     public void testCancelledTicketNotVerified() throws Exception {
-        Booking booking = bookingRepository.save(new Booking(3L, Booking.Status.CANCELLED, Set.of(new Ticket("1A")), "ramadistra@gmail.com", 12222));
+        Booking booking = createBooking(3L, Booking.Status.CANCELLED);
         this.mvc.perform(post("/tickets/" + booking.getId() + "/verify"))
                 .andExpect(jsonPath("$.status", is("CANCELLED")));
     }
