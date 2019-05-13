2263350c28a9279ead748cb0978b72659ea461b3
Raihan Ramadistra <ramadistra@gmail.com>
2019-05-13 16:52:54 +0700

    update ticket entity

--- ticket/src/main/java/com/adpro/ticket/model/Ticket.java
+++ ticket/src/main/java/com/adpro/ticket/model/Ticket.java
@@ -13,10 +13,12 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 @Entity
 @Data
 @NoArgsConstructor
+@Table(name="TICKET")
 public class Ticket {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
