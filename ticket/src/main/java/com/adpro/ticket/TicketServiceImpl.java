5c9a71d9b808bf0f9910cd7bd96d51cab6e02c59
Raihan Ramadistra <ramadistra@gmail.com>
2019-05-13 16:53:11 +0700

    add getBookingData

--- ticket/src/main/java/com/adpro/ticket/TicketServiceImpl.java
+++ ticket/src/main/java/com/adpro/ticket/TicketServiceImpl.java
@@ -1,5 +1,7 @@
 package com.adpro.ticket;
 
+import com.adpro.ticket.api.BookingData;
+import com.adpro.ticket.api.MovieService;
 import com.adpro.ticket.api.TicketRequestModel;
 import com.adpro.ticket.api.TicketService;
 import com.adpro.ticket.model.Booking;
@@ -9,20 +11,22 @@ import com.adpro.ticket.repository.TicketRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.Set;
+import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
 @Service
 public class TicketServiceImpl implements TicketService {
     private TicketRepository ticketRepository;
     private BookingRepository bookingRepository;
+    private MovieService movieService;
 
     @Autowired
-    public TicketServiceImpl(TicketRepository ticketRepository, BookingRepository bookingRepository) {
+    public TicketServiceImpl(TicketRepository ticketRepository, BookingRepository bookingRepository, MovieService movieService) {
         this.ticketRepository = ticketRepository;
         this.bookingRepository = bookingRepository;
+        this.movieService = movieService;
     }
 
     @Override
@@ -36,7 +40,7 @@ public class TicketServiceImpl implements TicketService {
             Set<Ticket> tickets = r.getSeatIds().stream()
                     .map(Ticket::new)
                     .collect(Collectors.toSet());
-            Booking booking = bookingRepository.save(new Booking(r.getSessionId(), Booking.Status.PENDING, tickets));
+            Booking booking = bookingRepository.save(new Booking(r.getSessionId(), Booking.Status.PENDING, tickets, r.getEmail(), r.getPrice()));
             return Optional.of(booking);
         }
         return Optional.empty();
@@ -57,5 +61,12 @@ public class TicketServiceImpl implements TicketService {
         }
 
         return Optional.of(booking);
+    }
+
+    @Override
+    public CompletableFuture<BookingData> getBookingData(Booking booking) {
+        return movieService.getMovieSessionById(booking.getSessionId()).thenApply(
+               session -> new BookingData(booking, session)
+        );
     }
 }
