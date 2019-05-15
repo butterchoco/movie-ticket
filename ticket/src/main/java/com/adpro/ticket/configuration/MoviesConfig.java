603b75fc1b5bfce2dcdbb0dc0403cdccd0c079a1
Raihan Ramadistra <ramadistra@gmail.com>
2019-05-15 10:13:26 +0700

    extend read timeout

--- ticket/src/main/java/com/adpro/ticket/configuration/MoviesConfig.java
+++ ticket/src/main/java/com/adpro/ticket/configuration/MoviesConfig.java
@@ -1,20 +1,28 @@
 package com.adpro.ticket.configuration;
 
 import com.adpro.ticket.api.movies.MovieService;
+import okhttp3.OkHttpClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
+
+import java.time.Duration;
 
 @Configuration
 public class MoviesConfig {
     @Bean
     public MovieService getMovieService() {
         final String BASE_URL_STRING = "http://ap-c8-movie.herokuapp.com/api/";
+        final OkHttpClient client = new OkHttpClient.Builder()
+            .readTimeout(Duration.ofMinutes(1))
+            .build();
+
         return new Retrofit.Builder()
-                .baseUrl(BASE_URL_STRING)
-                .addConverterFactory(JacksonConverterFactory.create())
-                .build()
-                .create(MovieService.class);
+            .baseUrl(BASE_URL_STRING)
+            .client(client)
+            .addConverterFactory(JacksonConverterFactory.create())
+            .build()
+            .create(MovieService.class);
     }
 }
